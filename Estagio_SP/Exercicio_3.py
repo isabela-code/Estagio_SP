6f78fc0d15b283992fcc6b926bdeb70109d067fd
Isabela de Queiroz <117490695+isabela-code@users.noreply.github.com>
2024-09-03 19:21:32 -0300

    Update Exercicio_3.py

--- Estagio_SP/Exercicio_3.py
+++ Estagio_SP/Exercicio_3.py
@@ -3,7 +3,7 @@ import json
 with open('faturamento.json', 'r') as arquivo:
     dados = json.load(arquivo)
 
-faturamento = dados['faturamento']
+faturamento = [dia['valor'] for dia in dados]
 faturamento_filtrado = [valor for valor in faturamento if valor > 0]
 menor_faturamento = min(faturamento_filtrado)
 maior_faturamento = max(faturamento_filtrado)
